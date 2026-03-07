aaaac0ae3330a4b3ff32112a0c30ad63fdbb88fe
Hamzar <60004028+xhamzar@users.noreply.github.com>
2026-03-08 06:46:46 +0700

    Log error if accessibility service is not ready
    Add error logging for null accessibility service.

--- app/src/main/java/com/termux/api/apis/AutoClickAPI.java
+++ app/src/main/java/com/termux/api/apis/AutoClickAPI.java
@@ -2,6 +2,7 @@ package com.termux.api.apis;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.termux.api.TermuxApiReceiver;
 import com.termux.api.services.AutoClickAccessibilityService;
@@ -18,8 +19,11 @@ public class AutoClickAPI {
         AutoClickAccessibilityService service =
                 AutoClickAccessibilityService.getInstance();
 
-        if(service != null){
-            service.click(x, y);
+        if(service == null){
+            Log.e("AutoClickAPI","Accessibility service not ready");
+            return;
         }
+
+        service.click(x,y);
     }
 }
