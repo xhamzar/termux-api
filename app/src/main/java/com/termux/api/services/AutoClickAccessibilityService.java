ae8f26a3428324fe8b3516ed77365a13c530f613
Hamzar <60004028+xhamzar@users.noreply.github.com>
2026-03-08 19:35:42 +0700

    Enhance AutoClickAccessibilityService functionality

--- app/src/main/java/com/termux/api/services/AutoClickAccessibilityService.java
+++ app/src/main/java/com/termux/api/services/AutoClickAccessibilityService.java
@@ -1,12 +1,15 @@
 package com.termux.api.services;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.accessibilityservice.GestureDescription;
 import android.graphics.Path;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
 public class AutoClickAccessibilityService extends AccessibilityService {
+
+    private static final String LOG_TAG = "AutoClickService";
 
     private static AutoClickAccessibilityService instance;
 
@@ -17,20 +20,40 @@ public class AutoClickAccessibilityService extends AccessibilityService {
     @Override
     protected void onServiceConnected(){
         super.onServiceConnected();
+
         instance = this;
 
-        Log.d("AutoClickService","Service connected");
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+
+        info.flags =
+                AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS |
+                AccessibilityServiceInfo.FLAG_RETRIEVE_INTERACTIVE_WINDOWS;
+
+        setServiceInfo(info);
+
+        Log.d(LOG_TAG,"Service connected");
     }
 
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event){}
+    public void onAccessibilityEvent(AccessibilityEvent event){
+        // tidak digunakan
+    }
 
     @Override
-    public void onInterrupt(){}
+    public void onInterrupt(){
+        Log.e(LOG_TAG,"Service interrupted");
+    }
 
     public void click(int x,int y){
 
-        Log.d("AutoClickService","Click "+x+" "+y);
+        if(instance == null){
+            Log.e(LOG_TAG,"Service instance NULL");
+            return;
+        }
+
+        Log.d(LOG_TAG,"Click "+x+" "+y);
 
         Path path = new Path();
         path.moveTo(x,y);
@@ -42,6 +65,22 @@ public class AutoClickAccessibilityService extends AccessibilityService {
                 new GestureDescription.StrokeDescription(path,0,50)
         );
 
-        dispatchGesture(builder.build(),null,null);
+        dispatchGesture(
+                builder.build(),
+                new GestureResultCallback(){
+
+                    @Override
+                    public void onCompleted(GestureDescription gestureDescription){
+                        Log.d(LOG_TAG,"Gesture completed");
+                    }
+
+                    @Override
+                    public void onCancelled(GestureDescription gestureDescription){
+                        Log.e(LOG_TAG,"Gesture cancelled");
+                    }
+
+                },
+                null
+        );
     }
 }
